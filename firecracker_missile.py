5f3d1d3b7b7620c6a80543d7d39337897216c71d
Timur Baev <timurbaev@gmail.com>
2018-03-29 17:20:42 +0000

    Firecracker weapon works

--- firecracker_missile.py
+++ firecracker_missile.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3.6
 
 from basic_missile import Basic_Missile
-from basic_blow import Basic_Blow
+from firecracker_blow import Firecracker_Blow
 
 class Firecracker_Missile(Basic_Missile):
 	def __init__(self,engine,x,y,power,angle):
 		super().__init__(engine,x,y,power,angle)
 		setattr(self, '_Basic_Missile__desctription', "Firecracker")
+		setattr(self, '_Basic_Missile__blow_class', Firecracker_Blow)
