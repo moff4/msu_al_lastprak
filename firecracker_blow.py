4ae25bc946d5fdbddb83bbfcfd7c2b8a80c4180b
Timur Baev <timurbaev@gmail.com>
2018-03-30 14:45:54 +0000

    Hot fix

--- firecracker_blow.py
+++ firecracker_blow.py
@@ -10,5 +10,6 @@ class Firecracker_Blow(Basic_Blow):
 	def __init__(self,engine,x,y):
 		super().__init__(engine,x,y)
 		setattr(self, '_Basic_Blow__desctription', conf.FirecrackerBlow_desctription)
+		parts = 10
 		for i in range(parts):
 			engine.add_missile_or_blow(firecracker_missile2.Firecracker_Missile2(engine,x,y,conf.FirecrackerBlow_SplinterSpeed,i * math.pi / conf.FirecrackerBlow_SplinterAmount))
